0429f8a332ac8a82b5007c5b965982a4ca62f9ae
C4RR0T02 <66990228+C4RR0T02@users.noreply.github.com>
2022-04-24 13:32:24 +0800

    Update Python as a calculator.py

--- Python as a calculator.py
+++ Python as a calculator.py
@@ -12,7 +12,7 @@ The code in the script gives some examples.
 
 Instructions: 
 Suppose you have $100, which you can invest with a 10% return each year. 
-After one year, it's  dollars, and after two years it's. 
+After one year, it's 100 X 1.1 = 110 dollars, and after two years it's 100 X 1.1 X 1.1 = 121. 
 Add code to calculate how much money you end up with after 7 years, and print the result.
 '''
 
