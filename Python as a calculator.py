dd5eaa029f97d77f22ba9964708c312c67fa78d2
C4RR0T02 <66990228+C4RR0T02@users.noreply.github.com>
2022-04-24 13:27:22 +0800

    Update Python as a calculator.py

--- Python as a calculator.py
+++ Python as a calculator.py
@@ -1,3 +1,21 @@
+'''
+Python is perfectly suited to do basic calculations. 
+Apart from addition, subtraction, multiplication and division, there is also support for more advanced operations such as:
+
+Exponentiation: **. This operator raises the number to its left to the power of the number to its right. 
+For example 4**2 will give 16.
+
+Modulo: %. This operator returns the remainder of the division of the number to the left by the number on its right. 
+For example 18 % 7 equals 4.
+
+The code in the script gives some examples.
+
+Instructions: 
+Suppose you have $100, which you can invest with a 10% return each year. 
+After one year, it's  dollars, and after two years it's. 
+Add code to calculate how much money you end up with after 7 years, and print the result.
+'''
+
 # Addition, subtraction
 print(5 + 5)
 print(5 - 5)
