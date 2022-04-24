c64bb8b8a34154c03a850c194890f34cfba195cb
C4RR0T02 <66990228+C4RR0T02@users.noreply.github.com>
2022-04-24 13:46:35 +0800

    Update Operations with other types.py

--- Operations with other types.py
+++ Operations with other types.py
@@ -1,3 +1,17 @@
+'''
+Hugo mentioned that different types behave differently in Python.
+
+When you sum two strings, for example, you'll get different behavior than when you sum two integers or two booleans.
+
+In the script some variables with different types have already been created. It's up to you to use them.
+
+Instructions:
+Calculate the product of savings and growth_multiplier. Store the result in year1.
+What do you think the resulting type will be? Find out by printing out the type of year1.
+Calculate the sum of desc and desc and store the result in a new variable doubledesc.
+Print out doubledesc. Did you expect this?
+'''
+
 savings = 100
 growth_multiplier = 1.1
 desc = "compound interest"
