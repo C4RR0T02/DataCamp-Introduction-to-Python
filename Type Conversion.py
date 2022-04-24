b417336d5a5ec1fd102ef97d16002d68bf473a74
C4RR0T02 <66990228+C4RR0T02@users.noreply.github.com>
2022-04-24 13:49:24 +0800

    Update Type Conversion.py

--- Type Conversion.py
+++ Type Conversion.py
@@ -1,3 +1,23 @@
+'''
+Using the + operator to paste together two strings can be very useful in building custom messages.
+
+Suppose, for example, that you've calculated the return of your investment and want to summarize the results in a string. 
+Assuming the integer savings and float result are defined, you can try something like this:
+
+print("I started with $" + savings + " and now have $" + result + ". Awesome!")
+This will not work, though, as you cannot simply sum strings and integers/floats.
+
+To fix the error, you'll need to explicitly convert the types of your variables. 
+More specifically, you'll need str(), to convert a value into a string. str(savings), for example, will convert the integer savings to a string.
+
+Similar functions such as int(), float() and bool() will help you convert Python values into any type.
+
+Instructions: 
+Hit Run Code to run the code. Try to understand the error message.
+Fix the code such that the printout runs without errors; use the function str() to convert the variables to strings.
+Convert the variable pi_string to a float and store this float as a new variable, pi_float.
+'''
+
 # Definition of savings and result
 savings = 100
 result = 100 * 1.10 ** 7
